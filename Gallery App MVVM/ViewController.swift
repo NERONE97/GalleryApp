e9f97412d17a4a0f85230143fead01877948e135
Roman <59637297+NERONE97@users.noreply.github.com>
2026-03-13 17:44:55 +0300

    [ADDED] Initial commit

---
+++ Gallery App MVVM/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Gallery App MVVM
+//
+//  Created by Roman on 13.03.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+    }
+
+
+}
+
+#Preview {
+    ViewController()
+}
